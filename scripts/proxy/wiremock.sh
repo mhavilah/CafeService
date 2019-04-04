4a59ea6892dd8c87c954eab306f85664fa9043b1
mhavilah <mhavilah@gmail.com>
2019-04-04 22:11:12 +1100

    Updated README.  Amended wiremock CLI with port argument

--- scripts/proxy/wiremock.sh
+++ scripts/proxy/wiremock.sh
@@ -4,7 +4,7 @@
 # Run Wiremock Server
 #
 # Usage:
-#      ./wiremock.sh
+#      ./wiremock.sh [port]
 #
 # As per:
 # http://wiremock.org/docs/running-standalone/
@@ -15,14 +15,41 @@
 ############################
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-PORT=8888
+usage ()
+{
+cat <<EOM
+#################
+#
+# Run Wiremock Server
+#
+# Usage:
+#      ./wiremock.sh [port]
+#
+# As per:
+# http://wiremock.org/docs/running-standalone/
+#
+# Files to serve are under: __files
+# URL Mappings are spec'd in: mappings/*.json
+#
+#################
+EOM
+}
+
+if [[ $# != 1 ]]
+then
+	usage
+	return 1 2>/dev/null || exit 1
+fi
+
+PORT=$1
 BASE_URL=http://localhost:$PORT
 
-WIREMOCK_JAR=wiremock-standalone.jar
+WIREMOCK_JAR=wiremock-standalone-2.19.0.jar
+WIREMOCK_DIR=$DIR/lib
 
-if [[ ! -f $DIR/$WIREMOCK_JAR && ! -L $DIR/$WIREMOCK_JAR ]]
+if [[ ! -f $WIREMOCK_DIR/$WIREMOCK_JAR && ! -L $WIREMOCK_DIR/$WIREMOCK_JAR ]]
 then
-	echo Cannot find Wiremock: $DIR/$WIREMOCK_JAR
+	echo Cannot find Wiremock: $WIREMOCK_DIR/$WIREMOCK_JAR
 	echo Exiting...
 	return 1 2>/dev/null || exit 1
 fi
@@ -30,7 +57,7 @@ fi
 #
 # Check process
 #
-if [[ $(ps -ef | grep java | grep wiremock | grep -v grep) != "" ]]
+if [[ $(ps -ef | grep java | grep wiremock | grep $PORT | grep -v grep) != "" ]]
 then
 	echo Wiremock is already up.
 	return 1 2>/dev/null || exit 1
@@ -38,7 +65,7 @@ fi
 
 echo Starting wiremock on port: $PORT...
 
-java -classpath $DIR -jar $DIR/$WIREMOCK_JAR --port $PORT --local-response-templating --print-all-network-traffic --root-dir $DIR &
+java -classpath $WIREMOCK_DIR -jar $WIREMOCK_DIR/$WIREMOCK_JAR --port $PORT --local-response-templating --print-all-network-traffic --root-dir $DIR &
 # http://wiremock.org/docs/running-standalone/
 # --match-headers: ...
 # --global-response-templating -  Render all response definitions using Handlebars templates.
